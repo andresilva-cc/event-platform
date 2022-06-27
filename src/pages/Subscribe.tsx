3c8f46e8ed203ce6c04e9250f9b185b3349e0467
André Silva <andreluiz.97@outlook.com>
2022-06-26 22:51:09 -0300

    fix: image not displaying on prod

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -2,6 +2,7 @@ import { useState, FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Logo } from '../components/Logo';
 import { useCreateSubscriberMutation } from '../graphql/generated';
+import codeMockupImage from '../assets/code-mockup.png';
 
 export function Subscribe() {
   const navigate = useNavigate()
@@ -68,7 +69,7 @@ export function Subscribe() {
           </form>
         </div>
       </div>
-      <img src="/src/assets/code-mockup.png" className="mt-10" alt="" />
+      <img src={codeMockupImage} className="mt-10" alt="" />
     </div>
   )
 }
